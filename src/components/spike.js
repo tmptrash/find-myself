c0be6649ee9f36f6bbb6cce6946a577949209d42
flatline <deadbrainman@gmail.com>
2025-10-23 23:57:57 +0300

    Delay

--- src/components/spike.js
+++ src/components/spike.js
@@ -107,7 +107,7 @@ export function loadSprites(k) {
  * @param {Object} inst - Spike instance
  * @param {number} delaySeconds - Delay before first appearance
  */
-export function startAnimation(inst, delaySeconds = 3) {
+export function startAnimation(inst, delaySeconds = 1) {
   const { spike, k } = inst
   
   // Wait for initial delay, then start animation cycle

9240701d56443cf09246da35e4047a8a0fd796f1
flatline <deadbrainman@gmail.com>
2025-10-27 23:49:04 +0200

    structure refactoring

--- src/sections/blades/level-1.4.js
+++ src/sections/blades/level-1.4.js
@@ -1,10 +1,10 @@
-import { CFG } from '../cfg.js'
-import { initScene, updateEerieSound } from '../utils/scene.js'
-import { getColor } from '../utils/helper.js'
-import * as Sound from '../utils/sound.js'
-import * as Spikes from '../components/spikes.js'
-import * as Hero from '../components/hero.js'
-import { createLightningState, updateLightning, drawLightning } from '../utils/connection.js'
+import { CFG } from '../../cfg.js'
+import { initScene, updateEerieSound } from '../../utils/scene.js'
+import { getColor } from '../../utils/helper.js'
+import * as Sound from '../../utils/sound.js'
+import * as Spikes from '../../components/spikes.js'
+import * as Hero from '../../components/hero.js'
+import { createLightningState, updateLightning, drawLightning } from '../../utils/connection.js'
 
 export function sceneLevel4(k) {
   k.scene("level-1.4", () => {
